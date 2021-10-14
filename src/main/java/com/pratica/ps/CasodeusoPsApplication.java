c9fbc00603ba7889ff30f224943e08cf45beacb0
MatheusMattos-stack <matheuzinhochaves@hotmail.com>
2021-10-13 21:15:10 -0300

    Projeto criado

---
+++ src/main/java/com/pratica/ps/CasodeusoPsApplication.java
@@ -0,0 +1,13 @@
+package com.pratica.ps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasodeusoPsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasodeusoPsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratica/ps/CasodeusoPsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratica.ps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasodeusoPsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
