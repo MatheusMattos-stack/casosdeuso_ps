d14d950988b18c3b4b3bdadd7c118bfe32a9ad10
MatheusMattos-stack <matheuzinhochaves@hotmail.com>
2021-10-20 21:16:51 -0300

    REST TESTADO

--- src/main/java/pratica/CasodeusoPsApplication.java
+++ src/main/java/pratica/CasodeusoPsApplication.java
@@ -1,4 +1,4 @@
-package com.pratica.ps;
+package pratica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/pratica/domain/Categoria.java
@@ -0,0 +1,28 @@
+package pratica.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import pratica.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<CategoriaResources> listar() {
+		
+		CategoriaResources cat1 = new CategoriaResources(1, "Informática");
+		CategoriaResources cat2 = new CategoriaResources(2, "Escritório");
+		
+		List<CategoriaResources> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+
+}
