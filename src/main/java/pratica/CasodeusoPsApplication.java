a99b0ce1de825afaeb164b9930b8d835a50a562f
MatheusMattos-stack <matheuzinhochaves@hotmail.com>
2021-11-03 21:02:56 -0300

    novo commit

--- src/main/java/pratica/CasodeusoPsApplication.java
+++ src/main/java/pratica/CasodeusoPsApplication.java
@@ -1,13 +1,28 @@
 package pratica;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CasodeusoPsApplication {
 
+
+
+@SpringBootApplication
+public class CasodeusoPsApplication implements CommandLineRunner{
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CasodeusoPsApplication.class, args);
+	}	
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório"); 
+
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
